daeb81d6a3c01d7b2ad1c1fac535ca82d4577394
Julia Desmazes <pitch@internet.com>
2026-06-01 10:47:21 -0500

    add tx skeleton

--- src/mac_tx.v
+++ src/mac_tx.v
@@ -2,7 +2,7 @@
 Copyright (c) 2026 Julia Desmazes, all rights reserved.  
 
 This code was written by a human, authorization is explicitly not 
-granted to use it to train any model. 
+granted for it to be used to train any model. 
 */
 
 `default_nettype none
@@ -16,7 +16,9 @@ Module will be adding :
 */
 module mac_tx #(
 	parameter PHY_W = 2,
-	localparam MAC_W = 48
+	localparam MAC_W = 48,
+	localparam ETHTYPE_W = 16,
+	parameter APP_ETHTYPE = 16'h88B6 
 )(
 	input clk, 
 	input wire rst_n, 
@@ -24,19 +26,24 @@ module mac_tx #(
 	input wire [MAC_W-1:0] phy_mac_i, 
 
 	input wire             data_v_i, 
+	input wire             data_last_i, 
 	input wire [PHY_W-1:0] data_i, 
 	input wire [MAC_W-1:0] data_dst_mac_i,
 	output wire            data_acc_o, // accept payload, start streaming
 
-	output wire phy_v_o,
+	output wire             phy_v_o,
 	output wire [PHY_W-1:0] phy_o	
 );
 localparam CNT_W = 6; 
-localparam [CNT_W-1:0] PREAMBLE_CNT = 7 * (8/PHY); 
-localparam [CNT_W-1:0] SFD_CNT      = 1 * (8/PHY); 
-localparam [CNT_W-1:0] MAC_CNT      = 6 * (8/PHY); 
-localparam [CNT_W-1:0] ETHTYPE_CNT  = 2 * (8/PHY); 
-localparam [CNT_W-1:0] FCS_CNT      = 4 * (8/PHY); 
+localparam [CNT_W-1:0] PREAMBLE_CNT = 7 * (8/PHY) - 1; 
+localparam [CNT_W-1:0] SFD_CNT      = 1 * (8/PHY) - 1; 
+localparam [CNT_W-1:0] MAC_CNT      = 6 * (8/PHY) - 1; 
+localparam [CNT_W-1:0] ETHTYPE_CNT  = 2 * (8/PHY) - 1; 
+localparam [CNT_W-1:0] FCS_CNT      = 4 * (8/PHY) - 1; 
+
+localparam [CNT_W-1:0] SFD_CNT_MIN_1 = 1 * (8/PHY) - 2; 
+
+localparam FCS_W = 32;
 /* 
 fsm
 start streaming out header when a new request arrived (data_v_i), 
@@ -46,7 +53,7 @@ When data_v_i is deaserted this signals the end of the payload
 so append the fcs */
 localparam IDLE     = 3'd0;
 localparam PREAMBLE = 3'd1;
-localparam SFD      = 3'd2; 
+localparam SFD      = 3'd2;// seperating preamble and sfd to save 16b of ff 
 localparam SRC_MAC  = 3'd3;
 localparam DST_MAC  = 3'd4;
 localparam ETHTYPE  = 3'd5;
@@ -69,17 +76,51 @@ always @(posedge clk)
 			FCS     : fsm_q <= cnt_q == FCS_CNT ? IDLE : FCS;
 		endcase
 	end
-end
 
 assign data_acc_o = (fsm_q == PAYLOAD);
 
-// mac
-reg [MAC_W-1:0] mac_buff_q;
-always @(posedge clk) 
-	if ((fsm_q == SFD) && (cnt_q == SFD_CNT)) mac_buff_q <= phy_mac_i;
-	else if ((fsm_q == SRC_MAC) && (cnt_q == MAC_CNT)) mac_buff_q <= data_dst_mac_i;
-	else mac_buff_q <= {mac_buff_q[MAC_W-PHY_W-1:0], data_i};
+// fcs 
+wire [FCS_W-1:0] pkt_fcs;
+crc m_fcs(
+	.clk(clk),
+	.rst_crc(fsm_q == SFD),
+	.data_in(phy_o),
+	.crc_en(1'b1),
+	.crc_out(pkt_fcs)
+);
 
-wire [PHY_W-1:0] data;
-assign data = ((fsm_q == SRC_MAC)|(fsm_q == DST_MAC))? mac_buff_q[MAC_W-1:MAC_W-3]:
+// output shift buffer
+localparam BUFF_W = MAC_W; // max(MAC_W, FCS, ETHTYPE)
+reg [BUFF_W-1:0] shift_buff_q;
+wire sel_src_mac;
+wire sel_dst_mac;
+wire sel_ethtype; 
+wire sel_fcs;
+
+assign sel_src_mac = (fsm_q == SFD) & (cnt_q == SFD_CNT);
+assign sel_dst_mac = (fsm_q == SRC_MAC) & (cnt_q == MAC_CNT);
+assign sel_ethtype = (fsm_q == DST_MAC) & (cnt_q == MAC_CNT);
+assign sel_fcs     = (fsm_q == PAYLOAD) & data_last_i;
+
+// TODO add a onehot0 attribute if yosys doesn't catch it automatically 
+always @(posedge clk) begin
+	casex ({sel_fcs, sel_ethtype, sel_dst_mac, sel_src_mac})
+		4'bXXX1: shift_buff_q <= phy_mac_i;
+		4'bXX1X: shift_buff_q <= data_dst_mac_i;
+		4'bX1XX: shift_buff_q <= {APP_ETHTYPE, {BUFF_W-ETHTYPE_W{1'bX}}};
+		4'b1XXX: shift_buff_q <= {fcs, {BUFF_W-FCS_W{1'bX}}};
+		default: shift_buff_q <= {shift_buff_q[BUFF_W-PHY_W-1-:PHY_W], {BUFF_W-PHY_W{1'bX}}}; 
+	endcase
+end
+
+wire [PHY_W-1:0] preamble_data; 
+wire sel_sfd_last;
+wire sel_preamble_sfd;
+
+assign sel_sfd_last     = (cnt_q == SFD_CNT_MIN_1);
+assign sel_preamble_sfd = (fsm_q == PREAMBLE) | (fsm_q == SFD);
+assign preamble_data = sel_sfd_last? 2'b11 : 2'b10; 
+
+assign phy_o = sel_preamble_sfd ? preamble_data: (fsm_q == PAYLOAD)? data_i: shift_buff_q[BUFF_W-1-:PHY_W];   
+assign phy_v_o = (fsm_q != IDLE);
 endmodule	
