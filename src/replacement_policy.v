5f14e5f9ff8d6b312ccdb1d15c121ed924ef0afc
Julia Desmazes <julia.desmazes@gmail.com>
2026-06-20 19:14:07 -0500

    fix replacement policy

--- src/replacement_policy.v
+++ src/replacement_policy.v
@@ -34,16 +34,20 @@ wire [TTNN_W-1:0] ttnn_lvl0[1:0];
 wire [IDX_W-1:0]  idx_lvl0[1:0];
 wire [IDX_W-1:0]  idx_lvl1;
 
-assign lvl0[1] = ttnn_i[4*TTNN_W-1-:TTNN_W]< ttnn_i[3*TTNN_W-1-:TTNN_W];
+assign lvl0[1]      = ttnn_i[4*TTNN_W-1-:TTNN_W]< ttnn_i[3*TTNN_W-1-:TTNN_W];
 assign ttnn_lvl0[1] = lvl0[1] ? ttnn_i[4*TTNN_W-1-:TTNN_W] : ttnn_i[3*TTNN_W-1-:TTNN_W];
 assign idx_lvl0[1]  = lvl0[1] ? 2'd3 : 2'd2;
 
-assign lvl0[0] = ttnn_i[2*TTNN_W-1-:TTNN_W]< ttnn_i[1*TTNN_W-1-:TTNN_W];
+assign lvl0[0]      = ttnn_i[2*TTNN_W-1-:TTNN_W]< ttnn_i[1*TTNN_W-1-:TTNN_W];
 assign ttnn_lvl0[0] = lvl0[0] ? ttnn_i[2*TTNN_W-1-:TTNN_W] : ttnn_i[1*TTNN_W-1-:TTNN_W];
-assign idx_lvl0[1]  = lvl0[1] ? 2'd1 : 2'd0;
+assign idx_lvl0[0]  = lvl0[0] ? 2'd1 : 2'd0;
 
 assign lvl1 = ttnn_lvl0[1] < ttnn_lvl0[0];
 assign idx_lvl1 = lvl1 ? idx_lvl0[1] : idx_lvl0[0];
+
+wire [IDX_W-1:0] debug_idx_lvl0_1, debug_idx_lvl0_0;
+assign debug_idx_lvl0_1 = idx_lvl0[1];
+assign debug_idx_lvl0_0 = idx_lvl0[0];
 
 reg [N-1:0] victime;
 always @(*) begin
